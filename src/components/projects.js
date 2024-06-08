e1e0971bfc2e46b08f965f2afbc55b320b51718a
isamsm <isamarsques@gmail.com>
2024-06-08 10:28:38 -0300

    feat: projetos em modelo de card

--- src/components/projects.js
+++ src/components/projects.js
@@ -16,226 +16,240 @@ export default function Projects() {
         {' '}
         Meus projetos{' '}
       </h1>
-      <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-5 font-semibold text-lg">
-        <div>
+      <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-5 font-semibold text-xl">
+        <div className="projects-card relative h-full w-full">
           <Image
             src={boleria}
             className="rounded-md"
             alt="Imagem da página inicial do meu projeto de boleria"
           />
-          <h5 className="pt-2"> CODE&BAKE </h5>
-          <div className="flex gap-2">
-            <a
-              title="Link repositório projeto CODE&BAKE"
-              className="transition transform hover:scale-125"
-              href="https://github.com/isamsm/CODE-BAKE"
-              target="_blank"
-            >
-              {' '}
-              <LuCode />{' '}
-            </a>
-            <a
-              title="Link projeto hospedado CODE&BAKE"
-              className="transition transform hover:scale-125"
-              href="https://code-bake.vercel.app"
-              target="_blank"
-            >
-              {' '}
-              <LuLink2 />{' '}
-            </a>
+          <div className="projects-info">
+            <h5 className="pt-2"> CODE&BAKE </h5>
+            <div className="flex gap-2">
+              <a
+                title="Link repositório projeto CODE&BAKE"
+                className="transition transform hover:scale-125"
+                href="https://github.com/isamsm/CODE-BAKE"
+                target="_blank"
+              >
+                {' '}
+                <LuCode />{' '}
+              </a>
+              <a
+                title="Link projeto hospedado CODE&BAKE"
+                className="transition transform hover:scale-125"
+                href="https://code-bake.vercel.app"
+                target="_blank"
+              >
+                {' '}
+                <LuLink2 />{' '}
+              </a>
+            </div>
           </div>
         </div>
-        <div>
+        <div className="projects-card relative h-full w-full">
           <Image
             src={escolha}
             className="rounded-md"
             alt="Imagem da página inicial do meu projeto de escolha"
           />
-          <h5 className="pt-2 font-semibold md:text-md lg:text-md">
-            {' '}
-            Escolha{' '}
-          </h5>
-          <div className="flex gap-2">
-            <a
-              title="Link repositório projeto Escolha"
-              className="transition transform hover:scale-125"
-              href="https://github.com/isamsm/escolha"
-              target="_blank"
-            >
+          <div className="projects-info">
+            <h5 className="pt-2 font-semibold md:text-md lg:text-md">
               {' '}
-              <LuCode />{' '}
-            </a>
-            <a
-              title="Link projeto hospedado Escolha"
-              className="transition transform hover:scale-125"
-              href="https://escolha.vercel.app"
-              target="_blank"
-            >
-              {' '}
-              <LuLink2 />{' '}
-            </a>
+              Escolha{' '}
+            </h5>
+            <div className="flex gap-2">
+              <a
+                title="Link repositório projeto Escolha"
+                className="transition transform hover:scale-125"
+                href="https://github.com/isamsm/escolha"
+                target="_blank"
+              >
+                {' '}
+                <LuCode />{' '}
+              </a>
+              <a
+                title="Link projeto hospedado Escolha"
+                className="transition transform hover:scale-125"
+                href="https://escolha.vercel.app"
+                target="_blank"
+              >
+                {' '}
+                <LuLink2 />{' '}
+              </a>
+            </div>
           </div>
         </div>
-        <div>
+        <div className="projects-card relative h-full w-full">
           <Image
             src={gameflix}
             className="rounded-md"
             alt="Imagem da página inicial do meu projeto do gameflix"
           />
-          <h5 className="pt-2 font-semibold md:text-md lg:text-md">
-            {' '}
-            Gameflix{' '}
-          </h5>
-          <div className="flex gap-2">
-            <a
-              title="Link repositório projeto Gameflix"
-              className="transition transform hover:scale-125"
-              href="https://github.com/isamsm/gameflix"
-              target="_blank"
-            >
+          <div className="projects-info">
+            <h5 className="pt-2 font-semibold md:text-md lg:text-md">
               {' '}
-              <LuCode />{' '}
-            </a>
-            <a
-              title="Link projeto hospedado Gameflix"
-              className="transition transform hover:scale-125"
-              href="https://gameflix-ashen.vercel.app"
-              target="_blank"
-            >
-              {' '}
-              <LuLink2 />{' '}
-            </a>
+              Gameflix{' '}
+            </h5>
+            <div className="flex gap-2">
+              <a
+                title="Link repositório projeto Gameflix"
+                className="transition transform hover:scale-125"
+                href="https://github.com/isamsm/gameflix"
+                target="_blank"
+              >
+                {' '}
+                <LuCode />{' '}
+              </a>
+              <a
+                title="Link projeto hospedado Gameflix"
+                className="transition transform hover:scale-125"
+                href="https://gameflix-ashen.vercel.app"
+                target="_blank"
+              >
+                {' '}
+                <LuLink2 />{' '}
+              </a>
+            </div>
           </div>
         </div>
       </div>
       <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-5 font-semibold text-lg">
-        <div>
+        <div className="projects-card relative h-full w-full">
           <Image
             src={auth}
             className="rounded-md"
             alt="Imagem da página inicial do meu projeto de Login/Cadastro"
           />
-          <h5 className="pt-2 font-semibold md:text-md lg:text-md">
-            {' '}
-            Authentification{' '}
-          </h5>
-          <div className="flex gap-2">
-            <a
-              title="Link repositório projeto Login/Cadastro"
-              className="transition transform hover:scale-125"
-              href="https://github.com/isamsm/authentification"
-              target="_blank"
-            >
+          <div className="projects-info">
+            <h5 className="pt-2 font-semibold md:text-md lg:text-md">
               {' '}
-              <LuCode />{' '}
-            </a>
-            <a
-              title="Link projeto hospedado Login/Cadastro"
-              className="transition transform hover:scale-125"
-              href="https://isamsm.github.io/authentification/"
-              target="_blank"
-            >
-              {' '}
-              <LuLink2 />{' '}
-            </a>
+              Authentification{' '}
+            </h5>
+            <div className="flex gap-2">
+              <a
+                title="Link repositório projeto Login/Cadastro"
+                className="transition transform hover:scale-125"
+                href="https://github.com/isamsm/authentification"
+                target="_blank"
+              >
+                {' '}
+                <LuCode />{' '}
+              </a>
+              <a
+                title="Link projeto hospedado Login/Cadastro"
+                className="transition transform hover:scale-125"
+                href="https://isamsm.github.io/authentification/"
+                target="_blank"
+              >
+                {' '}
+                <LuLink2 />{' '}
+              </a>
+            </div>
           </div>
         </div>
-        <div>
+        <div className="projects-card relative h-full w-full">
           <Image
             src={todo}
             className="rounded-md"
             alt="Imagem da página inicial do meu projeto de To-Do-list"
           />
-          <h5 className="pt-2 font-semibold md:text-md lg:text-md">
-            {' '}
-            To-Do-List{' '}
-          </h5>
-          <div className="flex gap-2">
-            <a
-              title="Link repositório projeto To-Do-List"
-              className="transition transform hover:scale-125"
-              href="https://github.com/isamsm/to-do-list"
-              target="_blank"
-            >
+          <div className="projects-info">
+            <h5 className="pt-2 font-semibold md:text-md lg:text-md">
               {' '}
-              <LuCode />{' '}
-            </a>
-            <a
-              title="Link projeto hospedado To-Do-List"
-              className="transition transform hover:scale-125"
-              href="https://isamsm.github.io/to-do-list/"
-              target="_blank"
-            >
-              {' '}
-              <LuLink2 />{' '}
-            </a>
+              To-Do-List{' '}
+            </h5>
+            <div className="flex gap-2">
+              <a
+                title="Link repositório projeto To-Do-List"
+                className="transition transform hover:scale-125"
+                href="https://github.com/isamsm/to-do-list"
+                target="_blank"
+              >
+                {' '}
+                <LuCode />{' '}
+              </a>
+              <a
+                title="Link projeto hospedado To-Do-List"
+                className="transition transform hover:scale-125"
+                href="https://isamsm.github.io/to-do-list/"
+                target="_blank"
+              >
+                {' '}
+                <LuLink2 />{' '}
+              </a>
+            </div>
           </div>
         </div>
-        <div>
+        <div className="projects-card relative h-full w-full">
           <Image
             src={mentoria}
             className="rounded-md"
             alt="Imagem da página inicial do meu projeto de mentoria"
           />
-          <h5 className="pt-2 font-semibold md:text-md lg:text-md">
-            {' '}
-            Mentoria{' '}
-          </h5>
-          <div className="flex gap-2">
-            <a
-              title="Link repositório projeto Mentoria"
-              className="transition transform hover:scale-125"
-              href="https://github.com/isamsm/mentoria"
-              target="_blank"
-            >
+          <div className="projects-info">
+            <h5 className="pt-2 font-semibold md:text-md lg:text-md">
               {' '}
-              <LuCode />{' '}
-            </a>
-            <a
-              title="Link projeto hospedado Mentoria"
-              className="transition transform hover:scale-125"
-              href="https://mentoria-zeta.vercel.app"
-              target="_blank"
-            >
-              {' '}
-              <LuLink2 />{' '}
-            </a>
+              Mentoria{' '}
+            </h5>
+            <div className="flex gap-2">
+              <a
+                title="Link repositório projeto Mentoria"
+                className="transition transform hover:scale-125"
+                href="https://github.com/isamsm/mentoria"
+                target="_blank"
+              >
+                {' '}
+                <LuCode />{' '}
+              </a>
+              <a
+                title="Link projeto hospedado Mentoria"
+                className="transition transform hover:scale-125"
+                href="https://mentoria-zeta.vercel.app"
+                target="_blank"
+              >
+                {' '}
+                <LuLink2 />{' '}
+              </a>
+            </div>
           </div>
         </div>
       </div>
       <div className="grid md:grid-cols-3 lg:grid-cols-3 gap-5 font-semibold text-lg">
-        <div>
+        <div className="projects-card relative h-full w-full">
           <Image
             src={amigo_literario}
             className="rounded-md"
             alt="Imagem da página inicial do meu projeto Amigo Literário"
           />
-          <h5 className="pt-2 font-semibold md:text-md lg:text-md">
-            {' '}
-            Amigo Literário{' '}
-          </h5>
-          <div className="flex gap-2">
-            <a
-              title="Link repositório projeto Amigo Literário"
-              className="transition transform hover:scale-125"
-              href="https://github.com/isamsm/amigo_literario"
-              target="_blank"
-            >
+          <div className="projects-info">
+            <h5 className="pt-2 font-semibold md:text-md lg:text-md">
               {' '}
-              <LuCode />{' '}
-            </a>
-            <a
-              title="Link projeto hospedado Amigo Literário"
-              className="transition transform hover:scale-125"
-              href="https://amigo-literario-theta.vercel.app"
-              target="_blank"
-            >
-              {' '}
-              <LuLink2 />{' '}
-            </a>
+              Amigo Literário{' '}
+            </h5>
+            <div className="flex gap-2">
+              <a
+                title="Link repositório projeto Amigo Literário"
+                className="transition transform hover:scale-125"
+                href="https://github.com/isamsm/amigo_literario"
+                target="_blank"
+              >
+                {' '}
+                <LuCode />{' '}
+              </a>
+              <a
+                title="Link projeto hospedado Amigo Literário"
+                className="transition transform hover:scale-125"
+                href="https://amigo-literario-theta.vercel.app"
+                target="_blank"
+              >
+                {' '}
+                <LuLink2 />{' '}
+              </a>
+            </div>
           </div>
         </div>
-        <div className="flex md:items-center lg:items-center">
+        <div className="flex md:items-center lg:items-center justify-center">
           <a
             title="Link outros projetos no Github"
             className="transition transform hover:scale-125"
