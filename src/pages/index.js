b0cd023e28befcf81764b1d80d3a1be76010dc65
isamsm <isamarsques@gmail.com>
2024-06-17 14:30:19 -0300

    fix: tentativa correção erro deploy

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import Home from '../components/Home'
+import Home from '../components/Home'  
 import About from '../components/About'
 import Projects from '../components/Projects'
 import Contact from '../components/Contact'
